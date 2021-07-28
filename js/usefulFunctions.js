a9512e79ab1fd8cace161c5f00ff0efb9e712f4d
Hamish Macfie <info@sealrock.co.uk>
2021-07-28 12:13:09 +0100

    Added a useful functions file with helpful reminders of code

---
+++ js/usefulFunctions.js
@@ -0,0 +1,15 @@
+// When needing to use white space from content delivered from a headless cms
+// new lines like \n or \n need replacing with a break.
+
+const exampleData = "<p>This is text \n with line breaks \r in";
+
+const replaced = exampleData.replace(/(?:\r\n|\r|\n)/g, "<br>");
+
+console.log(replaced);
+
+// Add css to make the breaks look better
+// br {
+//     display: block;
+//     content: "";
+//     margin-top: 1.5rem;
+// }
